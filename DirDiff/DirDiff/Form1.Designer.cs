7c842affc0caee3c0e95ecb4da0a79b7977081c2
罗昊 <luoh@mctech.vip>
2022-05-31 18:38:16 +0800

    初版

---
+++ DirDiff/DirDiff/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace DirDiff
+{
+    partial class DirDiffForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.path2 = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.path1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.listBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(1);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(867, 581);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.path2, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btn2, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(434, 1);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(1);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(432, 28);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // path2
+            // 
+            this.path2.AutoSize = true;
+            this.path2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.path2.Location = new System.Drawing.Point(63, 1);
+            this.path2.Margin = new System.Windows.Forms.Padding(3, 1, 0, 1);
+            this.path2.Name = "path2";
+            this.path2.Size = new System.Drawing.Size(369, 26);
+            this.path2.TabIndex = 2;
+            this.path2.Text = "Dir2";
+            this.path2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btn2
+            // 
+            this.btn2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn2.Location = new System.Drawing.Point(0, 0);
+            this.btn2.Margin = new System.Windows.Forms.Padding(0);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(60, 28);
+            this.btn2.TabIndex = 0;
+            this.btn2.Text = "目录2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(3, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(427, 545);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox1_DrawItem);
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 17;
+            this.listBox2.Location = new System.Drawing.Point(436, 33);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(428, 545);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox2_DrawItem);
+            this.listBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox2_KeyDown);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.btn1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.path1, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(1, 1);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(1);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(431, 28);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // btn1
+            // 
+            this.btn1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn1.Location = new System.Drawing.Point(0, 0);
+            this.btn1.Margin = new System.Windows.Forms.Padding(0);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(60, 28);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "目录1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // path1
+            // 
+            this.path1.AutoSize = true;
+            this.path1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.path1.Location = new System.Drawing.Point(63, 1);
+            this.path1.Margin = new System.Windows.Forms.Padding(3, 1, 0, 1);
+            this.path1.Name = "path1";
+            this.path1.Size = new System.Drawing.Size(368, 26);
+            this.path1.TabIndex = 1;
+            this.path1.Text = "Dir1";
+            this.path1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // DirDiffForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 581);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "DirDiffForm";
+            this.Text = "DirDiff";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label path2;
+        private System.Windows.Forms.Label path1;
+    }
+}
+
---
+++ DirDiff/DirDiff/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirDiff
+{
+    public partial class DirDiffForm : Form
+    {
+        Dictionary<string, bool> _names1;
+        Dictionary<string, bool> _names2;
+        public DirDiffForm()
+        {
+            _names1 = new Dictionary<string, bool>();
+            _names2 = new Dictionary<string, bool>();
+            InitializeComponent();
+            //this.listBox1.Items.AddRange(new List<string> { "1", "2" }.ToArray());
+            //this.listBox1.DataSource = new List<string> { "1", "2" };
+        }
+
+        private void listBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (listBox1.Items.Count <= 0)
+                return;
+            string k = listBox1.GetItemText(listBox1.Items[e.Index]);
+            
+            if (_names1.ContainsKey(k) && _names1[k] == false)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.LightPink), e.Bounds);                
+            }
+            else
+            {
+                e.Graphics.DrawRectangle(new Pen(Color.LightGreen, 2), e.Bounds);
+            }
+            e.Graphics.DrawString(k, this.Font, new SolidBrush(Color.Black), new PointF(e.Bounds.X, e.Bounds.Y));
+            
+        }
+
+        private void listBox2_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (listBox2.Items.Count <= 0)
+                return;
+            string k = listBox2.GetItemText(listBox2.Items[e.Index]);
+
+            if (_names2.ContainsKey(k) && _names2[k] == false)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.LightPink), e.Bounds);
+            }
+            else
+            {
+                e.Graphics.DrawRectangle(new Pen(Color.LightGreen, 2), e.Bounds);
+            }
+            e.Graphics.DrawString(k, this.Font, new SolidBrush(Color.Black), new PointF(e.Bounds.X, e.Bounds.Y));
+        }
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void listBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void SetList1(List<string> names1)
+        {
+            names1.Sort();
+            _names1 = names1.ToDictionary(k => k, v => true);
+            Compare();
+            listBox1.DataSource = _names1.Keys.ToList();
+            listBox1.Refresh();
+        }
+
+        private void SetList2(List<string> names2)
+        {
+            names2.Sort();
+            _names2 = names2.ToDictionary(k => k, v => true);
+            Compare();
+            listBox2.DataSource = _names2.Keys.ToList();
+            listBox2.Refresh();
+        }
+
+        private void Compare()
+        {
+            if (_names2.Any() && _names1.Any())
+            {
+                foreach (string k1 in _names1.Keys.ToList())
+                {
+                    _names1[k1] = _names2.Keys.Contains(k1);
+                }
+                foreach (string k2 in _names2.Keys.ToList())
+                {
+                    _names2[k2] = _names1.Keys.Contains(k2);
+                }
+                listBox1.Refresh();
+                listBox2.Refresh();
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new FolderBrowserDialog())
+            {
+                var ret = dlg.ShowDialog();
+                if (ret == DialogResult.OK)
+                {
+                    var dir = dlg.SelectedPath;
+                    this.path1.Text = dir;
+                    var files = Directory.GetFileSystemEntries(dir);
+                    var names = files.Select(x => Path.GetFileName(x)).ToList();
+                    SetList1(names);
+                }
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new FolderBrowserDialog())
+            {
+                var ret = dlg.ShowDialog();
+                if (ret == DialogResult.OK)
+                {
+                    var dir = dlg.SelectedPath;
+                    this.path2.Text = dir;
+                    var files = Directory.GetFileSystemEntries(dir);
+                    var names = files.Select(x => Path.GetFileName(x)).ToList();
+                    SetList2(names);
+                }
+            }
+        }
+    }
+}
